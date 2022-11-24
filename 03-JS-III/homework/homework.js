bf01016149bd1c40d12e74fe236e460a49bfe182
Romyhoyos <hoyosandrea977>
2022-11-24 15:58:43 -0300

    third commit

--- 03-JS-III/homework/homework.js
+++ 03-JS-III/homework/homework.js
@@ -8,29 +8,36 @@ function imprimirSumaNumeros() {
     // Imprime la suma de los números del 1 al 10
     // Pista: usa un acumulador
     // Tu código:
+    function imprimirSumaNumeros (a, b) {
+      for (var i= a + b; i<20; i++);
+    }
 }
 
 function encuentraPares(array){
   // Devuelve un arreglo con los pares encontrados
   // en el arreglo de enteros pasado como parámetro
   // Tu código:
+  for (var i= (a,b); i=numerosPares; i++);
 }
 
 function elevaAlCuadrado(array){
   // Devuelve un arreglo con cada número del array
   // elevado al cuadrado
   // Tu código:
+  return arrayelevaAlCuadrado;
 }
 
 function sumaArray(array){
   // Devuelve el resultado de sumar todos los elementos
   // de un arreglo de enteros dado
   // Tu código:
+  return suma=== a + b;
 }
 
 function numeroDigitos(num){
   // Devuelve el número de dígitos de un número dado
   // Tu código:
+  return numeroDigitos.length;
 }
   
   
